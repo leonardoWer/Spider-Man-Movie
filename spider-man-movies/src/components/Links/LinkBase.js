6fa29abc2291af689b203f099c410b01af6a64dd
leonardo_Wer <name89213126414@gmail.com>
2025-07-23 00:07:36 +0300

    Перенёс файлы в root

--- spider-man-movies/src/components/Links/LinkBase.js
+++
@@ -1,7 +0,0 @@
-export function LinkBase({ text, href, target = '_blank' }) {
-    const link = document.createElement('a');
-    text ? link.textContent = text : '';
-    href ? link.href = href : '';
-    link.target = target; // _blank, _self и т.д.
-    return link;
-}
